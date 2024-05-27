c4925844d1a542beb99ad7bed73d8425e2684920
Mandu <kooo1225@naver.com>
2024-05-27 11:40:01 +0900

    fix: FolderDTO 객체 date 자료형으로 수정

--- back/src/dto/FolderDTO.py
+++ back/src/dto/FolderDTO.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import date
 from typing import Union, Optional
+
 
 class FolderDTO(BaseModel):
     folder_name: str
     location_name: str
-    due_date: Optional[datetime]
+    due_date: Optional[date]
     marks: Optional[str]
 
 class FolderDTOinDB(FolderDTO):
