23067bce31a6f69a9c3d9d585b9a32285b777928
Mandu <kooo1225@naver.com>
2024-06-05 13:13:16 +0900

    feat: HwpParser Filter 구현

--- back/src/controller/HwpController.py
+++ back/src/controller/HwpController.py
@@ -12,7 +12,7 @@ PARSER_VERSION = {'간단이': SimpleParser() , '복잡이': ComplicatedParser()
 
 parser = APIRouter(prefix='/parser')
 service = HwpService()
-serailize_data = {}
+serialize_data = {}
 
 @parser.post('/', tags=['parser'])
 async def parsing(file: UploadFile = File(...), version: str = None, search_text: str = None):    
@@ -45,9 +45,34 @@ async def parsing(file: UploadFile = File(...), version: str = None, search_text
         table_cell.extend(service.set_table_cell(column_tag[index], target_tag[index]))
 
     os.remove(xml_path)
+    global serialize_data
     serialize_data = select_and_parsing(version, table_cell)
 
     return f'{serialize_data}'
+
+@parser.get('/location/{location_name}', tags=['parser'])
+def filter_location(location_name: str, version: str = None):
+    if not version:
+        return {'Error' : 'Not input version data'}
+
+    find_word = {'간단이' : '구분', '복잡이' : '측정위치', '어중이떠중이' : '계측위치'}
+    
+    global serialize_data
+    return [item for item in serialize_data if item[find_word[version]] == location_name]
+
+
+@parser.get('/locations/{version}', tags=['parser'])
+def get_locations(version: str = None):
+    if not version:
+        return {'Error' : 'Not input version data'}
+
+    find_word = {'간단이' : '구분', '복잡이' : '측정위치', '어중이떠중이' : '계측위치'}
+
+    global serialize_data
+    locations = [item[find_word[version]] for item in serialize_data]
+    unique_locations = [{'location_name': item} for item in list(set(locations))]
+
+    return unique_locations
 
 def select_and_parsing(version, table_cell):
     selected_parser = PARSER_VERSION[version]
